ede107e875442cf79e5bceed4a76506acc8d2a08
cup113 <3529706495@qq.com>
2022-03-22 18:53:45 +0800

    2.0.2 (Beta) | Change lib

--- src/init.ts
+++ src/init.ts
@@ -1,3 +1,3 @@
 /// <reference path="./footer.ts"/>
-const Version = "2.0.1 (Beta)";
-generate_footer(Version, "https://github.com/cup113/BMI-Calculator/", true);
+const Version = "2.0.2 (Beta)";
+generate_footer(Version, "https://github.com/cup113/BMI-Calculator/", true, 2022, 2021);
