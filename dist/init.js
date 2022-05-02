8a0771096e068249899fbccbbb5c57131d9a7a36
cup113 <3529706495@qq.com>
2022-05-02 22:49:49 +0800

    2.0.9 (Release)

--- dist/init.js
+++ dist/init.js
@@ -1,3 +1,3 @@
 /// <reference path="../Lib/footer.js"/>
-var Version = "2.0.7 (Release)";
+var Version = "2.0.9 (Release)";
 generate_footer(Version, "https://github.com/cup113/BMI-Calculator/", true, true, 2022, 2021);
